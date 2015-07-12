064c0dbd4039a8ac3e60772bb39fe8512aea33cb
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-07-12 14:22:07 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = "GetProjects",
+    py_modules = ["getprojects"],
+    entry_points = {"console_scripts": ["getprojects=getprojects:main"]},
+    version = "0.1.0",
+    description = "Clone all your GitHub repos with one command.",
+    author = "Gokberk Yaltirakli",
+    author_email = "webdosusb@gmail.com",
+    url = "https://github.com/gkbrk/GetProjects",
+    keywords = ["github", "git", "repo"],
+    requires = ["requests"]
+)
